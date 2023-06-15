84c861ab172a0e4bfd5ff558a748952cc9140e07
Marcus Davanço <md.daves@gmail.com>
2023-06-14 23:18:02 -0300

    feat: Funções e procedimentos

--- linguagem_c_luis_damas/chapter_05/prog0519.c
+++ linguagem_c_luis_damas/chapter_05/prog0519.c
@@ -1,5 +1,5 @@
+
 #include <stdio.h>
-#include <ctype.h>
 
 int isdigit(char c);
 int isalpha(char c);
@@ -10,38 +10,58 @@ int isspace(char c);
 char tolower(char c);
 char toupper(char c);
 
-main()
+int main()
 {
+    printf("toupper d = %c", toupper('d'));
+    return 0;
 }
 
 int isdigit(char c)
 {
+    return (c >= '0' && c <= '9');
 }
 
 int isalpha(char c)
 {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 int isalnum(char c)
 {
+    return isalpha(c) || isdigit(c);
 }
 
 int islower(char c)
 {
+    return (c >= 'a' && c <= 'z');
 }
 
 int isupper(char c)
 {
+    return (c >= 'A' && c <= 'Z');
 }
 
 int isspace(char c)
 {
+    return (c == ' ' || c == '\t'); /* Space or TAB */
 }
 
 char tolower(char c)
 {
+    if (isupper(c))
+    {
+        return c + 'a' - 'A';
+    }
+    else
+    {
+        return c;
+    }
 }
 
 char toupper(char c)
 {
+    if (islower(c))
+        return c + 'A' - 'a';
+    else
+        return c;
 }
